7a8a3ddc20088011068d17bc50e00ed816ddf32b
Mahammad Shukur <MahammadS@risk.az>
2026-04-28 19:02:17 +0400

    ok

---
+++ Expeditious/Expeditious.Common/code/id_counters/GeneralCounter.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Expeditious.Common
+{
+    public static class GeneralCounter
+    {
+        private static int _counter;
+
+        public static void SetStartNubber(int startFromThis) => Interlocked.Exchange(ref _counter, startFromThis - 1);
+
+        public static int Next() => Interlocked.Increment(ref _counter);
+
+        public static int Current => Volatile.Read(ref _counter);
+
+        public static void Reset() => Interlocked.Exchange(ref _counter, 0);
+    }
+}
+
+
